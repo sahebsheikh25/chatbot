61bbff5d15eb50f540a4437f1adf360d9af16f2f
sahebsheikh25 <sksohel46671@gmail.com>
2026-01-06 00:47:10 +0530

    card bg for course

--- public/courses.js
+++ public/courses.js
@@ -6,7 +6,7 @@ const products = [
     price: 0,
     category: "ai",
     desc: "7h 46m • ⭐ 4.97 • 1638 students",
-    img: "assets/images/courses/ai.jpg",
+    img: "/assets/images/courses/ai.png",
     amazonUrl: "https://www.udemy.com/course/complete-ai-agent-practical-course-c-aipc/?couponCode=NEW_YEAR_2026"
   },
   {
